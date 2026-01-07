2b108d52c3e9a72da712b9fc6e0834e884d77de0
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2026-01-06 22:03:12 -0300

    aula 03 - classes, metodos e atributos

--- bin/logica_prog_dart.dart
+++ bin/logica_prog_dart.dart
@@ -1,29 +1,31 @@
+class Celular {
+  //construtor
+  final String cor; //atributos
+  final int qntdProcessador; //final quer dizer que a variavel so pode receber um valor uma unica vez
+  final double tamanho;
+  final double peso;
+
+  Celular(this.cor,
+      this.qntdProcessador,
+      this.tamanho,
+      this.peso);
+
+  String toString() {
+    // metodo
+    return "Cor $cor, Processadores $qntdProcessador, Tamanho $tamanho, Peso $peso ";
+  }
+
+  double valor(double valor){
+    return valor*qntdProcessador;
+  }
+}
+
+
 void main() {
+  Celular celularApple = Celular('Roxo', 5, 0.5, 6);
+  Celular celularSamsung = Celular('Preto', 6, 0.7, 8);
 
-  bool seguirEmFrente = true;
-
-  if(seguirEmFrente){
-    print("Andar");
-  }else{
-    print("Parar");
-  }
-
-  if(10 > 5) {
-    print("10 eh maior que 5");
-  }
-
-  int valorInt = 3;
-  switch (valorInt) {
-    case 0:
-      print("zero");
-      break;
-    case 1:
-      print("um");
-      break;
-    case 2:
-      print("dois");
-      break;
-    default:
-      print("padrao");
-  }
+  print(celularApple.toString());
+  print(celularSamsung.toString());
+  print(celularApple.valor(1000));
 }
