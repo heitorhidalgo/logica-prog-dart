ae5acb0a04cbfb4820126a08383c0999a5ca662f
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2026-01-08 21:17:14 -0300

    aula 12 - enums

--- bin/logica_prog_dart.dart
+++ bin/logica_prog_dart.dart
@@ -1,10 +1,26 @@
-extension ExtensionString on String {
-  String toFirstCharToUpperCase() {
-    return this[0].toUpperCase() + this.substring(1);
+class Pagamento {
+  void pagar(TipoPagamento tipoPagamento) {
+    if (tipoPagamento.value == "PIX") {
+      print("Pagando com pix");
+    } else if (tipoPagamento.value == "BOLETO") {
+      print("Pagando com boleto");
+    } else if (tipoPagamento.value == "CARTAO") {
+      print("Pagando com cartao");
+    }
   }
 }
 
+enum TipoPagamento {
+  PIX("PIX"),
+  BOLETO("BOLETO"),
+  CARTAO("CARTAO");
+
+  final String value;
+
+  const TipoPagamento(this.value);
+}
+
 void main() {
-  String name = 'heitor';
-  print(name.toFirstCharToUpperCase()); //basta chamar o extension
+  var pagamento = Pagamento();
+  pagamento.pagar(TipoPagamento.CARTAO);
 }
